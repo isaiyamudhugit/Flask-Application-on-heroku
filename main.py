e0ad5bb41b983a566507ed11e3ae0837e15ad426
Isaiyamudhugit <isaiyamudhu@yahoo.co.in>
2021-07-08 19:34:04 +0530

    updated app- second commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-    return "<h1>This is flask application. working on heroku cloud platform</h1>"
+    return "<h1>This is flask application. working on heroku cloud platform. welldone!</h1>"
 
 if  __name__ == "__main__":
     app.run()
